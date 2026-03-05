66996da91ffd90c1f6a911caa770ec67a2c9a5a5
Balasubramanian0077 <iyyappanbalu605@gmail.com>
2026-03-05 20:27:59 +0530

    Update BASE_URL to production endpoint

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://mcq-backend-f1oz.onrender.com";
 
 async function apiFetch(path, options = {}) {
   const res = await fetch(BASE_URL + path, {
